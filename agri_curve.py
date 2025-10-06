5db2437723a9260b30c93c7d6dae372fc85ff6b3
nasserboan <nasserboan@gmail.com>
2025-10-06 20:42:19 -0300

    Update pipeline logic and increase resources to 2Gi

--- agri_curve.py
+++ agri_curve.py
@@ -1,9 +1,12 @@
 from src.pipelines.datagen import DataGenFlow
+import os
 
+pod_name = os.getenv('POD_NAME')
 
 def main():
     data_gen = DataGenFlow()
+    print(f"Pod name: {pod_name}")
     data_gen.run()
-
+    
 if __name__ == "__main__":
     main()
